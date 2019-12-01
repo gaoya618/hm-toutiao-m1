b59836574a02193a3421174084edc94931bb22f2
高雅 <gaoya@gaoyadeMacBook-Pro.local>
2019-12-01 09:48:31 +0800

    项目初始化

---
+++ src/views/layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 公共组件部分 -->
+  <div class="container">
+    <!-- 导航栏 -->
+    <!-- 内容容器 -->
+    <!-- tab栏 -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'layout',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang='less' scoped>
+</style>
